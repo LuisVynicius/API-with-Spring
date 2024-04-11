ea63790b70825fdd843dfe12e933e0b8c54d08f5
LuisVynicius <luismevy@gmail.com>
2024-04-11 19:35:16 -0300

    Create UserDetailsServiceImpl and JWTUtil

--- src/main/java/com/mevy/restfulapi/repositories/UserRepository.java
+++ src/main/java/com/mevy/restfulapi/repositories/UserRepository.java
@@ -1,9 +1,14 @@
 package com.mevy.restfulapi.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.mevy.restfulapi.models.User;
 
+
 public interface UserRepository extends JpaRepository<User, Long>{
     
+    @Transactional(readOnly = true)
+    User findByUsername(String username);
+
 }
---
+++ src/main/java/com/mevy/restfulapi/services/UserDetailsServiceImpl.java
@@ -0,0 +1,30 @@
+package com.mevy.restfulapi.services;
+
+import java.util.Objects;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.mevy.restfulapi.models.User;
+import com.mevy.restfulapi.repositories.UserRepository;
+import com.mevy.restfulapi.security.UserSpringSecurity;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService{
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (Objects.isNull(user)){
+            throw new UsernameNotFoundException("User not found: " + username);
+        }
+        return new UserSpringSecurity(user.getId(), user.getUsername(), user.getPassword(), user.getProfiles());
+    }
+    
+}
